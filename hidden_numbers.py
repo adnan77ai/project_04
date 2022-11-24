13f5b35ffc79f36fdb6a411eaf8c98e7b0917a9b
adnan77ai <yasinadnan79@gmail.com>
2022-11-24 13:22:04 +0600

    nothing changed

--- hidden_numbers.py
+++ hidden_numbers.py
@@ -1,7 +1,7 @@
-no=int(input('any number: '))
-numbers=[11,33,55,39,55,75,37,21,23,41,13]
+no = int(input('any number: '))
+numbers = [11,33,55,39,55,75,37,21,23,41,13]
 for num in numbers:
- if num==no:
+ if num == no:
     print ('number found in list')
     break
 else:
